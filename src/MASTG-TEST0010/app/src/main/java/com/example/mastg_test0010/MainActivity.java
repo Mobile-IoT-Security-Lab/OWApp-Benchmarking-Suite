a5caa674efe85c7f77ca5fd52fc403be79cafe92
luca959 <luca.ferrari.pro@gmail.com>
2024-03-20 12:23:38 +0100

    add test 10

---
+++ src/MASTG-TEST0010/app/src/main/java/com/example/mastg_test0010/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mastg_test0010;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
