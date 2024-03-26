39772106630393387de2c0344765b17068ff3700
luca959 <luca.ferrari.pro@gmail.com>
2024-03-26 15:54:26 +0100

    add test 9

--- src/MASTG-TEST0009/app/src/main/java/com/example/mastg_test_0009/MainActivity.java
+++ src/MASTG-TEST0009/app/src/main/java/com/example/mastg_test_0009/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.mastg_test_0009;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText text1 = findViewById(R.id.editTextText);
+        EditText text2 = findViewById(R.id.editTextTextPassword);
+        Button register = findViewById(R.id.button);
+        Button login = findViewById(R.id.lg);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String u = text1.getText().toString();
+                String p = text2.getText().toString();
+                if (u.equals("") || p.equals("")) {
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast.makeText(MainActivity.this, "Fill the Form", duration).show();
+                    return;
+                }
+                saveCredentialsToFile(u, p);
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    private void saveCredentialsToFile(String username, String password) {
+        FileOutputStream fos = null;
+        try {
+            // Open the file in append mode
+            fos = openFileOutput("credentials.txt", MODE_APPEND);
+            String data = "Username: " + username + " Password: " + password + "\n"; // Add newline for separation
+            fos.write(data.getBytes());
+            Toast.makeText(MainActivity.this, "Credentials saved to file", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, "Error saving credentials", Toast.LENGTH_SHORT).show();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
