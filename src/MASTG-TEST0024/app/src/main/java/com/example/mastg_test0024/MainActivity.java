410ea76b98d5438c664ea47fd5b9abbfc5c7105b
luca959 <luca.ferrari.pro@gmail.com>
2024-03-27 16:56:02 +0100

    add test 24

--- src/MASTG-TEST0024/app/src/main/java/com/example/mastg_test0024/MainActivity.java
+++ src/MASTG-TEST0024/app/src/main/java/com/example/mastg_test0024/MainActivity.java
@@ -1,14 +1,116 @@
 package com.example.mastg_test0024;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 101;
+    private static final int CALL_PHONE_PERMISSION_REQUEST_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button takePhoto= findViewById(R.id.photo_button);
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.CAMERA,Manifest.permission.CALL_PHONE}, CAMERA_PERMISSION_REQUEST_CODE);
+
+                } else {
+                    dispatchTakePictureIntent();
+                }
+            }
+        });
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Camera permission granted
+                // You can start camera-related operations here
+            } else {
+                // Camera permission denied
+                Toast.makeText(this, "Camera permission is required to use the camera.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap imageBitmap= null;
+        ImageView click_image_id = findViewById(R.id.imageView);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            click_image_id.setImageBitmap(imageBitmap);
+            String timeStamp1 = new SimpleDateFormat("yyyy/MM/dd_HH:mm:ss").format(new Date());
+            saveToInternalStorage(imageBitmap);
+        }
+
+    }
+
+    public void saveToInternalStorage(Bitmap view){
+        ImageView click_image_id = findViewById(R.id.imageView);
+        BitmapDrawable bd=(BitmapDrawable) click_image_id.getDrawable();
+        Bitmap b=bd.getBitmap();
+        // ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        Log.d("saveToInternalStorage: ",getApplicationContext().getFilesDir().toString());
+        //File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        File path= getApplicationContext().getFilesDir();
+        // Create imageDir
+        File my_path=new File(path,"profile.png");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(my_path);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            b.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //return path.getAbsolutePath();
     }
 }
