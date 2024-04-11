fd42fceab6f2208bed2e8696bb37914d39bfe88c
luca959 <luca.ferrari.pro@gmail.com>
2024-04-11 16:29:57 +0200

    add test28

--- src/MASTGTEST0013/app/src/main/java/com/example/mastg_test0013/Decrypt.java
+++ src/MASTGTEST0013/app/src/main/java/com/example/mastg_test0013/Decrypt.java
@@ -1,15 +1,28 @@
 package com.example.mastg_test0013;
 
 import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Decrypt extends AppCompatActivity {
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
+public class Decrypt extends AppCompatActivity {
+    private static byte[] keyBytes ;
+    static{
+        Decrypt.keyBytes= new byte[] {7, 3, 4, 5, 6, 7, 8, 9, 16, 17, 18, 9, 20, 21, 15, 1};
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +33,34 @@ public class Decrypt extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        EditText dc= findViewById(R.id.editTextText2);
+        TextView resDec= findViewById(R.id.decResult);
+        Button decB= findViewById(R.id.button3);
+        decB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dc.getText().toString().equals("")){
+                    Toast.makeText(Decrypt.this, "Fill the form", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    resDec.setText(decryptData(dc.getText().toString().getBytes()));
+                }
+            }
+        });
+    }
+    private String decryptData(byte[] encryptedData) {
+        try {
+            SecretKeySpec secretKeySpec = new SecretKeySpec(Decrypt.keyBytes, "AES"); // Hardcoded key
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+            return new String(cipher.doFinal(Base64.decode(encryptedData,0)));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
