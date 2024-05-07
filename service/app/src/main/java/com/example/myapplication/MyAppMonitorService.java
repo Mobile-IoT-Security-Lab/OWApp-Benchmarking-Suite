90986260560463b60a816f031362ff11bc01fdee
luca959 <luca.ferrari.pro@gmail.com>
2024-05-07 22:42:35 +0200

    add overlay

--- service/app/src/main/java/com/example/myapplication/MyAppMonitorService.java
+++ service/app/src/main/java/com/example/myapplication/MyAppMonitorService.java
@@ -3,14 +3,21 @@ package com.example.myapplication;
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 
 public class MyAppMonitorService extends AccessibilityService {
+    private WindowManager windowManager;
+    private View overlayView;
 
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
         configureService(); // Example method for configuring the service
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
     }
 
     private void configureService() {
@@ -28,10 +35,38 @@ public class MyAppMonitorService extends AccessibilityService {
             // Check if the package name of the launched app matches your target app
             String packageName = String.valueOf(event.getPackageName());
             Log.d("AppMonitor", "app"+packageName+" is launched");
+            // Check if the launched app's package name matches your target app
+            if ("com.example.mastg_test0011".equals(packageName)) {
+                // Show overlay
+                showOverlay();
+            }
         }
+    }
+    private void showOverlay() {
+        if (windowManager == null) {
+            Log.e("AppMonitor", "WindowManager is null. Cannot add overlay.");
+            return;
+        }
+
+        // Inflate overlay layout
+        LayoutInflater inflater = LayoutInflater.from(this);
+        overlayView = inflater.inflate(R.layout.activity_overlay, null);
+
+        // Add overlay view to window manager
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                android.graphics.PixelFormat.TRANSLUCENT
+        );
+
+        windowManager.addView(overlayView, params);
     }
 
     @Override
-    public void onInterrupt() {}
+    public void onInterrupt() {
+
+    }
 
 }
