f290ecd4ff2342f52ce5ba2cc525f9b35fc70058
luca959 <luca.ferrari.pro@gmail.com>
2024-05-21 14:27:39 +0200

    review test 1

--- src/MASTG-TEST0046/app/src/main/java/com/example/mastg_test0046/SecondActivity.java
+++ src/MASTG-TEST0046/app/src/main/java/com/example/mastg_test0046/SecondActivity.java
@@ -1,9 +1,11 @@
 package com.example.mastg_test0046;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +22,10 @@ public class SecondActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Toolbar toolbar=findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        WebView view= findViewById(R.id.web);
+        view.loadUrl("https://www.example.com");
     }
 }
