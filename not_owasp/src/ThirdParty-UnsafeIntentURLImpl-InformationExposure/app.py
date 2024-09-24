a713ad8c7b051f52fa095dcf838c3daceff08aa7
Luca <luca.ferrari.pro@gmail.com>
2024-09-24 14:55:28 +0200

    Remove test not usable

--- not_owasp/src/ThirdParty-UnsafeIntentURLImpl-InformationExposure/app.py
+++
@@ -1,7 +0,0 @@
-#nella editText della mobile app mettere 10.0.2.2:5000
-from flask import Flask,request,render_template
-
-app = Flask(__name__)
-@app.route('/', methods=['GET'])
-def start():
-    return render_template('intentSender.html')
